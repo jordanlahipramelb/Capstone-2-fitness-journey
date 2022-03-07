55db99030fb21402b799ec569041d838cc173ce6
Jordan Lahip-Ramelb <jordan.lahipramelb@gmail.com>
2022-03-07 10:21:41 -0800

    update

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,6 @@
 /** Express app for Fitness Journey */
 
 const express = require("express");
-const path = require("path");
 const cors = require("cors");
 const { NotFoundError } = require("./expressError");
 const { authenticateJWT } = require("./middleware/auth");
@@ -11,6 +10,7 @@ const morgan = require("morgan");
 const app = express();
 
 /** Routes */
+
 const authRoutes = require("./routes/authRoutes");
 const usersRoutes = require("./routes/usersRoutes");
 const exercisesRoutes = require("./routes/exercisesRoutes");
@@ -18,16 +18,13 @@ const postsRoutes = require("./routes/postsRoutes");
 const postsCommentsRoutes = require("./routes/postsCommentsRoutes");
 const routineRoutes = require("./routes/routinesRoutes");
 const logRoutes = require("./routes/logsRoutes");
+
 /*******/
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(authenticateJWT);
-
-app.get("/", (req, res) => {
-  res, send("Hello world");
-});
 
 app.use("/auth", authRoutes);
 app.use("/athletes", usersRoutes);
@@ -39,11 +36,13 @@ app.use("/routines", routineRoutes);
 app.use("/logs", logRoutes);
 
 /** Handle 404 errors -- this matches everything */
+
 app.use((req, res, next) => {
   return next(new NotFoundError());
 });
 
 /** Generic error handler; anything unhandled goes here. */
+
 app.use((err, req, res, next) => {
   if (process.env.NODE_ENV !== "test") console.error(err.stack);
   const status = err.status || 500;
